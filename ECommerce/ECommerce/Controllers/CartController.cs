c91b1b2be180f9485e723925d18847edc7266255
Tausif Fardin <tausiffardin4@gmail.com>
2022-02-13 23:10:00 +0600

    Commit E-commerce project

---
+++ ECommerce/ECommerce/Controllers/CartController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using ECommerce.Models.Database;
+
+namespace ECommerce.Controllers
+{
+    public class CartController : Controller
+    {
+        // GET: Cart
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Buy(int id)
+        {
+            ProductEntities2 db = new ProductEntities2();
+            var p = (from data in db.Products
+                     where data.ProductId == id
+                     select data).FirstOrDefault();
+            if (Session["cart"] == null)
+            {
+                string json = new JavaScriptSerializer().Serialize(p);
+                Session["cart"] = json;
+            }
+            else
+            {
+                string json = Session["cart"].ToString();
+                var d = new JavaScriptSerializer().Deserialize<List<Product>>(json);
+                Session["cart"] = d.Add(p);
+            }
+            return RedirectToAction("ViewCart");
+        }
+        public ActionResult ViewCart()
+        {
+            return View();
+        }
+    }
+}
---
+++ ECommerce/ECommerce/Controllers/ProductController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using ECommerce.Models.Database;
+
+namespace ECommerce.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            ProductEntities2 db = new ProductEntities2();
+            var data = db.Products.ToList();
+            return View(data);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View(new Product());
+        }
+        [HttpPost]
+        public ActionResult Create(Product p)
+        {
+            if (ModelState.IsValid)
+            {
+                ProductEntities2 db = new ProductEntities2();
+                db.Products.Add(p);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            ProductEntities2 db = new ProductEntities2();
+            var product = (from s in db.Products
+                           where s.ProductId == id
+                           select s).FirstOrDefault();
+            return View(product);
+
+        }
+        [HttpPost]
+        public ActionResult Edit(Product sub_s)
+        {
+            ProductEntities2 db = new ProductEntities2();
+            var product = (from s in db.Products
+                           where s.ProductId == sub_s.ProductId
+                           select s).FirstOrDefault();
+            db.Entry(product).CurrentValues.SetValues(sub_s);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            ProductEntities2 db = new ProductEntities2();
+            var product = (from s in db.Products
+                           where s.ProductId == id
+                           select s).FirstOrDefault();
+            return View(product);
+
+        }
+        [HttpPost]
+        public ActionResult Delete(Product sub_s)
+        {
+            ProductEntities2 db = new ProductEntities2();
+            var product = (from s in db.Products
+                           where s.ProductId == sub_s.ProductId
+                           select s).FirstOrDefault();
+            db.Products.Remove(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult AddToCart(int id)
+        {
+            ProductEntities2 db = new ProductEntities2();
+            var p = (from data in db.Products
+                     where data.ProductId == id
+                     select data).FirstOrDefault();
+            if (Session["cart"] = null)
+            {
+                string json = new JavaScriptSerializer().Serialize(p);
+                Session["cart"] = json;
+            }
+        }
+    }
+}
