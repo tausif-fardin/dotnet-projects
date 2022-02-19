c090ec82b8915a59da83391fb7f6884a7326e0a0
Tausif Fardin <tausiffardin4@gmail.com>
2022-02-19 23:44:24 +0600

    commit

--- AssociationDemo/AssociationDemo/Controllers/HomeController.cs
+++ AssociationDemo/AssociationDemo/Controllers/HomeController.cs
@@ -12,9 +12,9 @@ namespace AssociationDemo.Controllers
         public ActionResult Index()
         {
             UMSEntities1 db = new UMSEntities1();
-            var dept = (from di in db.Departments
-                       where di.Id == 1
-                       select di).FirstOrDefault();
+            var dept = (from d in db.Departments
+                       where d.Id == 1
+                       select d).FirstOrDefault();
 
             var courses = dept.Courses.ToList();
             var students = dept.Students.ToList();
