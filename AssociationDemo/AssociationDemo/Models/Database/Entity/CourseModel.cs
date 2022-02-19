4e7e155b19c4ebe606904479654bd9ee8cb5577c
Tausif Fardin <tausiffardin4@gmail.com>
2022-02-20 00:00:32 +0600

    DepartmentCourseModel Add

---
+++ AssociationDemo/AssociationDemo/Models/Database/Entity/CourseModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AssociationDemo.Models.Database.Entity
+{
+    public class CourseModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int OfferedBy { get; set; }
+
+        public DepartmentModel Department { get; set; }
+    }
+}
---
+++ AssociationDemo/AssociationDemo/Models/Database/Entity/DepartmentCourseModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AssociationDemo.Models.Database.Entity
+{
+    public class DepartmentCourseModel : DepartmentModel
+    {
+        List<CourseModel> Courses = new List<CourseModel>();
+
+        public DepartmentCourseModel()
+        {
+            Courses = new List<CourseModel>(); 
+        }
+    }
+}
