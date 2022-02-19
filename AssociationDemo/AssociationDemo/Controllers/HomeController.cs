2df0e848dec217771d4709dcb12caac2db9fc781
Tausif Fardin <tausiffardin4@gmail.com>
2022-02-19 23:41:59 +0600

    Update association

--- AssociationDemo/AssociationDemo/Controllers/HomeController.cs
+++ AssociationDemo/AssociationDemo/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AssociationDemo.Models.Database;
 
 namespace AssociationDemo.Controllers
 {
@@ -10,6 +11,14 @@ namespace AssociationDemo.Controllers
     {
         public ActionResult Index()
         {
+            UMSEntities1 db = new UMSEntities1();
+            var dept = (from di in db.Departments
+                       where di.Id == 1
+                       select di).FirstOrDefault();
+
+            var courses = dept.Courses.ToList();
+            var students = dept.Students.ToList();
+
             return View();
         }
 
